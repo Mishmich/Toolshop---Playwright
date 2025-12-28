48c364c6a8388b8229119bb13b74d1f480a1f930
MissHell <michaela.mickova111@gmail.com>
2025-12-28 17:05:43 +0100

    contact - added tags

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,39 +1,38 @@
-import {test, expect} from '@playwright/test';
+import { test } from '@playwright/test';
 import * as contact from './pages/Contact.ts';
-import path from 'path';
 
 
-test.beforeEach(async ({page}) => {
-    await page.goto('/contact');
+test.beforeEach(async ({page, baseURL}) => {
+    await page.goto(`${baseURL}/contact`);
 });
 
-test('Submit fully filled out contact form', async ({page}) => {
+test('Submit fully filled out contact form', { tag: '@positive'} , async ({page}) => {
     await contact.fillOutContactForm(page);
     await contact.submitContactForm(page);
     await contact.checkSubmissionText(page);
 });
 
-test('Submit empty contact form', async ({page, baseURL}) => {
+test('Submit empty contact form', { tag: '@negative'}, async ({page, baseURL}) => {
     await contact.submitContactForm(page);
     await contact.checkValidationErrors(page, ['first-name', 'last-name', 'email', 'subject', 'message']);
-    await expect(page).toHaveURL(`${baseURL}/contact`);
+    await contact.checkUrl(page, baseURL!);
 });
 
-test('Submit contact form with invalid email', async ({page}) => {
+test('Submit contact form with invalid email', { tag: '@negative'}, async ({page}) => {
     await contact.fillOutContactForm(page);
     await page.locator('[data-test="email"]').fill('invalid-email');
     await contact.submitContactForm(page);
     await contact.checkValidationErrors(page, ['email']);
 });
 
-test('Submit contact form with valid attachment', async ({page}) => {
+test('Submit contact form with valid attachment', { tag: '@positive'}, async ({page}) => {
     await contact.fillOutContactForm(page);
     await contact.addAttachment(page, 'tests/data/attachments/validAttachmentFile.txt');
     await contact.submitContactForm(page);
     await contact.checkSubmissionText(page);
 });
 
-test('Submit contact form with invalid attachment', async ({page}) => {
+test('Submit contact form with invalid attachment', { tag: '@negative'}, async ({page}) => {
     await contact.fillOutContactForm(page);
     await contact.addAttachment(page, 'tests/data/attachments/invalidAttachmentFile.txt');
     await contact.submitContactForm(page);
