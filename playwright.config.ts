99b09fc40d7af2a104427c2c542c2d220460f935
MissHell <michaela.mickova111@gmail.com>
2026-01-05 13:29:17 +0100

    phoenix reporting fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],
-            ['github']],
+            ['github'],
+            ['junit', { outputFile: 'test-results/junit.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
