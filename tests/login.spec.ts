970ae125ea91c6d741b741eae25eb028dfbb9ca4
MissHell <michaela.mickova111@gmail.com>
2026-01-11 21:02:59 +0100

    added a simple login and related functions

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,15 @@
 import {test, expect} from '@playwright/test';
+import * as login from './pages/Login.ts';
 
 test.beforeEach(async ({page}) => {
     await page.goto('/auth/login');
 });
 
 test('Sign in with valid credentials', async ({page}) => {
-
+    await login.fillOutSignInForm(page);
+    await login.submitSignInForm(page);
+    const userProfile = page.locator('[data-test="nav-menu"]');
+    await expect(userProfile).toBeVisible();
 });
 
 test('Sign in with invalid credentials', async ({page}) => {
--- tests/pages/Login.ts
+++ tests/pages/Login.ts
@@ -1,5 +1,32 @@
 import { Page } from "@playwright/test";
 
-export async function fillOutSignInForm(page: Page) {
+export const user1 = {
+    email: 'customer3@practicesoftwaretesting.com',
+    password: 'pass123'
+};
 
+export async function fillOutSignInForm(page: Page) {
+    await page.locator('[data-test="email"]').fill(user1.email);
+    await page.locator('[data-test="password"]').fill(user1.password);
+}
+
+export async function submitSignInForm(page: Page) {
+    await page.locator('[data-test="login-submit"]').click();
+}
+
+export async function loginUser(page: Page, baseURL: string) {
+    await page.goto(`${baseURL}/auth/login`);
+    await fillOutSignInForm(page);
+    await submitSignInForm(page);
+    await page.waitForLoadState("networkidle");
+    await page.waitForLoadState("domcontentloaded");
+}
+
+export async function removeAllFaves(page: Page) {
+    await page.goto('/account/favorites');
+
+    while (await page.locator('[data-test="delete"]').count() > 0) {
+        await page.locator('[data-test="delete"]').first().click();
+        await page.waitForLoadState("networkidle");
+    }
 }
