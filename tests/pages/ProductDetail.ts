e27a5d762019bf6c91eace5319acbedf305957a7
MissHell <michaela.mickova111@gmail.com>
2026-01-11 21:01:50 +0100

    added product detail tests

--- tests/pages/ProductDetail.ts
+++ tests/pages/ProductDetail.ts
@@ -1,3 +1,54 @@
+import { Page, expect } from "@playwright/test";
+import { getProductId } from "./Home";
 
+export async function goToProductDetail(page: Page, baseURL: string, index: number) {
+    await page.goto('/');
+    const productId = await getProductId(page, baseURL, index);
+    await page.locator(`[data-test="product-${productId}"]`).click();
+}
 
+export async function addItemsToCart(page: Page) {
+    await page.locator('[data-test="add-to-cart"]').click();
+}
 
+export async function increaseItemQuantity(page: Page) {
+    await page.locator('[data-test="increase-quantity"]').click();
+}
+
+export async function decreaseItemQuantity(page: Page) {
+    await page.locator('[data-test="decrease-quantity"]').click();
+}
+
+export async function addItemToFavorites(page: Page) {
+    await page.locator('[data-test="add-to-favorites"]').click();
+}  
+
+export async function clickOnRelatedProduct(page: Page, index: number) {
+    await page.locator('css=a.card').nth(index).click();
+    await page.waitForLoadState("domcontentloaded");
+    await page.waitForLoadState("networkidle");
+}
+
+export async function checkProductAddedAlert(page: Page) {
+    let alertText = await page.getByRole('alert', { name: 'Product added to shopping' }).getAttribute('aria-label');
+    await expect(alertText).toContain('Product added to shopping cart.');
+}
+
+export async function getRelatedProductName(page: Page, index: number): Promise<{name: string | null, href: string | null}> {
+    const relatedProduct = page.locator('css=a.card').nth(index);
+    const name = await relatedProduct.locator('css=.card-title').textContent();
+    const href = await relatedProduct.getAttribute('href');
+    return {name, href};
+}
+
+export async function checkRelatedProductNavigation(page: Page, name: string, href: string) {
+    const productTitle = await page.locator('[data-test="product-name"]').textContent();
+    const currentURL = page.url();
+    await expect(productTitle).toBe(name);
+    await expect(currentURL).toContain(href);
+}
+
+export async function checkProductAddedToFavAlert(page: Page) {
+    let alertText = await page.getByRole('alert', { name: 'Product added to your favorites list.' }).textContent();
+    await expect(alertText).toContain('Product added to your favorites list.');
+}
--- tests/product-detail.spec.ts
+++ tests/product-detail.spec.ts
@@ -1,20 +1,55 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import * as productDetail from "./pages/ProductDetail.ts";
+import { loginUser, removeAllFaves } from "./pages/Login.ts";
 
-test.beforeEach(async ({page}) => {
-    await page.goto('/');
+test.describe("Unauthorized user tests", () => {
+  test.beforeEach(async ({ page, baseURL }) => {
+    await page.goto("/");
+    await productDetail.goToProductDetail(page, baseURL!, 0); // Navigate to the first product detail page
+  });
+  test("Add item to cart", async ({ page }) => {
+    await productDetail.addItemsToCart(page);
+    await productDetail.checkProductAddedAlert(page);
+  });
+
+  test("Decrease item quantity", async ({ page }) => {
+    await productDetail.increaseItemQuantity(page);
+    await productDetail.decreaseItemQuantity(page);
+    const quantity = await page.locator('[data-test="quantity"]').inputValue();
+    expect(quantity).toBe("1");
+    await productDetail.addItemsToCart(page);
+    await productDetail.checkProductAddedAlert(page);
+  });
+
+  test("Add item to favorites - unauthorized", async ({ page }) => {
+    await productDetail.addItemToFavorites(page);
+    const alertText = await page
+      .getByRole("alert", {
+        name: "Unauthorized, can not add product to your favorite list.",
+      })
+      .innerText();
+    expect(alertText).toContain(
+      "Unauthorized, can not add product to your favorite list."
+    );
+  });
+
+  test("Click on related product", async ({ page }) => {
+    const { name, href } = await productDetail.getRelatedProductName(page, 0);
+    await productDetail.clickOnRelatedProduct(page, 0);
+    await productDetail.checkRelatedProductNavigation(page, name!, href!);
+  });
 });
 
-test('Add item to cart', async ({ page }) => {
-  await page.locator('[data-test="product-01K8VB9PKMARMVTFH8EXQRPNR9"]').click();
-  await page.locator('[data-test="add-to-cart"]').click();
-  let alertText = await page.getByRole('alert', { name: 'Product added to shopping' }).getAttribute('aria-label');
-  await expect(alertText).toContain('Product added to shopping cart.');
+test.describe("Authorized user tests", () => {
+  test.beforeEach(async ({ page, baseURL }) => {
+    await loginUser(page, baseURL!);
+    await removeAllFaves(page);
+    await productDetail.goToProductDetail(page, baseURL!, 0);
+  });
+
+  test("Add item to favorites - authorized", async ({ page }) => {
+    // User is already logged in from beforeEach
+    await productDetail.addItemToFavorites(page);
+    await productDetail.checkProductAddedToFavAlert(page);
+  });
 });
-
-test('Decrease item quantity', async ({ page }) => {});
-
-test('Add item to favorites - unauthorized', async ({ page }) => {});
-
-test('Add item to favorites - authorized', async ({ page }) => {});
-
-test('Click on related product', async ({ page }) => {});
