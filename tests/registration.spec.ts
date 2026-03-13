c39c782fe942709a8114d7803ceee869a7a100e9
MissHell <michaela.mickova111@gmail.com>
2026-03-13 12:51:24 +0100

    registration wait added

--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -1,41 +1,65 @@
-import {test, expect} from '@playwright/test';
-import * as registration from './pages/Registration.ts';
-import {user1} from './pages/Login.ts';
+import { test, expect } from "@playwright/test";
+import * as registration from "./pages/Registration.ts";
+import { user1 } from "./pages/Login.ts";
 
-test.beforeEach(async ({page, baseURL}) => {
-    await page.goto(`${baseURL}/auth/register`);
+test.beforeEach(async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}/auth/register`);
 });
 
-test('Register a client with valid data', { tag: ['@positive', '@smoke'] }, async ({page}) => {
+test(
+  "Register a client with valid data",
+  { tag: ["@positive", "@smoke"] },
+  async ({ page }) => {
     await registration.fillRegistrationForm(page);
+    await page.waitForTimeout(1000);
     await registration.clickOnSubmitButton(page);
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login');
-});
+    await page.waitForTimeout(1000);
+    await expect(page).toHaveURL(
+      "https://practicesoftwaretesting.com/auth/login",
+    );
+  },
+);
 
-test('Submit empty registration form', { tag: '@negative'}, async ({page}) => {
+test(
+  "Submit empty registration form",
+  { tag: "@negative" },
+  async ({ page }) => {
     await registration.clickOnSubmitButton(page);
-    await expect(page.locator('.alert')).toHaveCount(11);
-});
+    await expect(page.locator(".alert")).toHaveCount(11);
+  },
+);
 
-test('Register with already used email', { tag: '@negative'}, async ({page}) => {
+test(
+  "Register with already used email",
+  { tag: "@negative" },
+  async ({ page }) => {
     await registration.fillRegistrationForm(page);
     await page.locator('[data-test="email"]').click();
     await page.locator('[data-test="email"]').fill(user1.email);
     await registration.clickOnSubmitButton(page);
-    await expect(page.locator('.alert')).toHaveText('A customer with this email address already exists.');
-});
+    await expect(page.locator(".alert")).toHaveText(
+      "A customer with this email address already exists.",
+    );
+  },
+);
 
-test('Register with invalid password', { tag: '@negative'}, async ({page}) => {
+test(
+  "Register with invalid password",
+  { tag: "@negative" },
+  async ({ page }) => {
     await registration.fillRegistrationForm(page);
     await page.locator('[data-test="password"]').click();
-    await page.locator('[data-test="password"]').fill('pass123');
+    await page.locator('[data-test="password"]').fill("pass123");
     await registration.clickOnSubmitButton(page);
-    await expect(page.locator('.alert')).toHaveText(' Password must be minimal 6 characters long.  Password must include invalid characters. ');
-});
+    await expect(page.locator(".alert")).toHaveText(
+      " Password must be minimal 6 characters long.  Password must include invalid characters. ",
+    );
+  },
+);
 
-test('Make password visible', { tag: '@positive'}, async ({page}) => {
-    await page.locator('[data-test="password"]').fill('ValidPass123');
-    await page.locator('button.btn.btn-outline-secondary').click();
-    const passwordField = page.locator('[data-test="password"]');
-    await expect(passwordField).toHaveAttribute('type', 'text');
+test("Make password visible", { tag: "@positive" }, async ({ page }) => {
+  await page.locator('[data-test="password"]').fill("ValidPass123");
+  await page.locator("button.btn.btn-outline-secondary").click();
+  const passwordField = page.locator('[data-test="password"]');
+  await expect(passwordField).toHaveAttribute("type", "text");
 });
